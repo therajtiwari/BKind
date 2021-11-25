866da02a3e43fe08757985526820fee4f4d494c7
trigya7701 <rwikroy123@gmail.com>
2021-11-25 15:26:28 +0530

    Register page

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -1,167 +1,245 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import '../utils/widget_functions.dart';
 import "../utils/constants.dart";
 
-class Register extends StatelessWidget {
-  const Register({Key? key}) : super(key: key);
+class Register extends StatefulWidget {
+  // const Register({Key? key}) : super(key: key);
 
+  // our form key
+  @override
+  _RegisterState createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  final _formKey = GlobalKey<FormState>();
+
+  final firstNameEditingController = new TextEditingController();
+
+  final countryEditingController = new TextEditingController();
+
+  final emailEditingController = new TextEditingController();
+
+  final passwordEditingController = new TextEditingController();
+
+  final confirmPasswordEditingController = new TextEditingController();
+
+  final languageEditingController = new TextEditingController();
+
+  final timeEditingController = new TextEditingController();
+  dynamic _value = 1;
   @override
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
+
+    //Name
+    final firstNameField = TextFormField(
+        autofocus: false,
+        controller: firstNameEditingController,
+        keyboardType: TextInputType.name,
+        onSaved: (value) {
+          firstNameEditingController.text = value!;
+        },
+        textInputAction: TextInputAction.next,
+        decoration: InputDecoration(
+          prefixIcon: Icon(Icons.account_circle),
+          contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+          hintText: "First Name",
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ));
+
+    //country field
+    final countryField = TextFormField(
+        autofocus: false,
+        controller: countryEditingController,
+        keyboardType: TextInputType.name,
+        onSaved: (value) {
+          countryEditingController.text = value!;
+        },
+        textInputAction: TextInputAction.next,
+        decoration: InputDecoration(
+          prefixIcon: Icon(Icons.flag_sharp),
+          contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+          hintText: "Country",
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ));
+
+    //email field
+    final emailField = TextFormField(
+        autofocus: false,
+        controller: emailEditingController,
+        keyboardType: TextInputType.emailAddress,
+        onSaved: (value) {
+          firstNameEditingController.text = value!;
+        },
+        textInputAction: TextInputAction.next,
+        decoration: InputDecoration(
+          prefixIcon: Icon(Icons.mail),
+          contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+          hintText: "Email",
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ));
+
+    //password field
+    final passwordField = TextFormField(
+        autofocus: false,
+        controller: passwordEditingController,
+        obscureText: true,
+        onSaved: (value) {
+          firstNameEditingController.text = value!;
+        },
+        textInputAction: TextInputAction.next,
+        decoration: InputDecoration(
+          prefixIcon: Icon(Icons.vpn_key),
+          contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+          hintText: "Password",
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ));
+
+    //confirm password field
+    final confirmPasswordField = TextFormField(
+        autofocus: false,
+        controller: confirmPasswordEditingController,
+        obscureText: true,
+        onSaved: (value) {
+          confirmPasswordEditingController.text = value!;
+        },
+        textInputAction: TextInputAction.next,
+        decoration: InputDecoration(
+          prefixIcon: Icon(Icons.vpn_key),
+          contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+          hintText: "Confirm Password",
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ));
+
+    final languageField = TextFormField(
+        autofocus: false,
+        controller: languageEditingController,
+        keyboardType: TextInputType.name,
+        onSaved: (value) {
+          languageEditingController.text = value!;
+        },
+        textInputAction: TextInputAction.done,
+        decoration: InputDecoration(
+          prefixIcon: Icon(Icons.language),
+          contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+          hintText: "Preferred Language",
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ));
+    final timeField = DropdownButtonFormField(
+      value: _value,
+      items: [
+        DropdownMenuItem(
+          child: Text("First Item"),
+          value: 1,
+        ),
+        DropdownMenuItem(
+          child: Text("Second Item"),
+          value: 2,
+        )
+      ],
+      onChanged: (value) {
+        setState(() {
+          _value = value;
+        });
+      },
+    );
+
+    //signup button
+    final signUpButton = Material(
+      elevation: 5,
+      borderRadius: BorderRadius.circular(10),
+      color: colorDarkBlue,
+      child: MaterialButton(
+          padding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+          minWidth: MediaQuery.of(context).size.width,
+          onPressed: () {},
+          child: Text(
+            "Register",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold),
+          )),
+    );
+
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: SingleChildScrollView(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: colorDarkBlue),
+          onPressed: () {
+            // passing this to our root
+            Navigator.of(context).pop();
+          },
+        ),
+      ),
+      body: Center(
+        child: SingleChildScrollView(
           child: Container(
-        margin: const EdgeInsets.only(left: 20.0, right: 20.0),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              addVerticalSpace(height * 0.15),
-              const Text(
-                'Hi there, Kind Person',
-                style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black),
-              ),
-              addVerticalSpace(height * 0.05),
-              const Text(
-                'Register to continue',
-                style: TextStyle(
-                  fontSize: 25,
-                  color: Colors.black,
+            color: Colors.white,
+            child: Padding(
+              padding: const EdgeInsets.all(36.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    SizedBox(
+                        height: 50,
+                        child: Text('Hi there, Kind Person',
+                            style: TextStyle(
+                                fontSize: 30,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black))),
+                    SizedBox(
+                        height: 50,
+                        child: Text('Register to continue',
+                            style: TextStyle(
+                              fontSize: 25,
+                              fontWeight: FontWeight.normal,
+                              color: Colors.black,
+                            ))),
+                    SizedBox(height: 20),
+                    firstNameField,
+                    SizedBox(height: 20),
+                    countryField,
+                    SizedBox(height: 20),
+                    emailField,
+                    SizedBox(height: 20),
+                    passwordField,
+                    SizedBox(height: 20),
+                    confirmPasswordField,
+                    SizedBox(height: 20),
+                    languageField,
+                    SizedBox(height: 20),
+                    timeField,
+                    signUpButton,
+                    SizedBox(height: 15),
+                  ],
                 ),
               ),
-              Column(
-                children: [
-                  Container(
-                    margin: const EdgeInsets.only(top: 20),
-                    child: const TextField(
-                      decoration: InputDecoration(
-                        labelText: 'Name',
-                        border: OutlineInputBorder(),
-                      ),
-                    ),
-                  ),
-                  Container(
-                    margin: const EdgeInsets.only(top: 20),
-                    child: const TextField(
-                      decoration: InputDecoration(
-                        labelText: 'Country',
-                        border: OutlineInputBorder(),
-                      ),
-                    ),
-                  ),
-                  Container(
-                    margin: const EdgeInsets.only(top: 20),
-                    child: const TextField(
-                      decoration: InputDecoration(
-                        labelText: 'Email',
-                        border: OutlineInputBorder(),
-                      ),
-                    ),
-                  ),
-                  Container(
-                    margin: const EdgeInsets.only(top: 20),
-                    child: const TextField(
-                      decoration: InputDecoration(
-                        labelText: 'Password',
-                        border: OutlineInputBorder(),
-                      ),
-                    ),
-                  ),
-                  Container(
-                    margin: const EdgeInsets.only(top: 20),
-                    child: const TextField(
-                      decoration: InputDecoration(
-                        labelText: 'Confirm Password',
-                        border: OutlineInputBorder(),
-                      ),
-                    ),
-                  ),
-                  Container(
-                    margin: const EdgeInsets.only(top: 20),
-                    child: const TextField(
-                      decoration: InputDecoration(
-                        labelText: 'Preffered Language',
-                        border: OutlineInputBorder(),
-                      ),
-                    ),
-                  ),
-                  Container(
-                    margin: const EdgeInsets.only(top: 20),
-                    child: const TextField(
-                      decoration: InputDecoration(
-                        labelText: 'Other Language',
-                        border: OutlineInputBorder(),
-                      ),
-                    ),
-                  ),
-                  Container(
-                    // margin: const EdgeInsets.only(top: 20),
-                    margin: const EdgeInsets.only(top: 20),
-                    child: const TextField(
-                      decoration: InputDecoration(
-                        labelText: 'Free Time',
-                        border: OutlineInputBorder(),
-                      ),
-                    ),
-                  ),
-                  addVerticalSpace(30),
-                  SizedBox(
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        primary: colorDarkBlue,
-                        onPrimary: Colors.white,
-                        // minimumSize: const Size(350, 50),
-                        minimumSize: const Size.fromHeight(60),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(5),
-                        ), //////// HERE
-                      ),
-                      onPressed: () {
-                        Navigator.pushNamed(context, 'auth/auth/post');
-                      },
-                      child: const Text(
-                        'Register',
-                        style: TextStyle(
-                          fontSize: 20,
-                        ),
-                      ),
-                    ),
-                  ),
-                  addVerticalSpace(20),
-
-                  // padding: const EdgeInsets.only(right: 20.0),
-                  SizedBox(
-                    // add margin
-
-                    width: double.infinity,
-                    child: InkWell(
-                      onTap: () {
-                        Navigator.pushNamed(context, "/auth/register");
-                      },
-                      child: const Text(
-                        "Login",
-                        textAlign: TextAlign.right,
-                        style: TextStyle(
-                          fontSize: 18,
-                          color: Colors.black,
-                        ),
-                      ),
-                    ),
-                  ),
-
-                  addVerticalSpace(30),
-                ],
-              )
-            ],
+            ),
           ),
         ),
-      )),
+      ),
     );
   }
 }
