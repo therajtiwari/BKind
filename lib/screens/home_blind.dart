15d50c95533cf7874e75d38e4156184ea004d6f6
trigya7701 <rwikroy123@gmail.com>
2021-12-03 01:45:26 +0530

    Added App Drawer and fixed back buttton

--- lib/screens/home_blind.dart
+++ lib/screens/home_blind.dart
@@ -1,6 +1,7 @@
 import 'package:bkind/models/user_model.dart';
 import 'package:bkind/provider/user_provider.dart';
 import 'package:bkind/screens/callscreens/pickup/pickup_layout.dart';
+import 'package:bkind/screens/profile.dart';
 import 'package:bkind/utils/call_utilis.dart';
 import 'package:bkind/utils/constants.dart';
 import 'package:bkind/utils/permissions.dart';
@@ -10,6 +11,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:provider/provider.dart';
+
+import 'login.dart';
 
 class HomeBlind extends StatefulWidget {
   const HomeBlind({Key? key}) : super(key: key);
@@ -43,6 +46,8 @@ class _HomeBlind extends State<HomeBlind> {
       userSince: '',
       isVol: false);
 
+  var body;
+
   @override
   void initState() {
     super.initState();
@@ -75,59 +80,118 @@ class _HomeBlind extends State<HomeBlind> {
 // class HomeBlind extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('bKind'),
-          backgroundColor: colorDarkBlue,
-        ), //AppBar
-        body: SingleChildScrollView(
-          child: Column(children: [
-            Container(
-              alignment: Alignment.topCenter,
-              margin: const EdgeInsets.only(
-                left: 7.0,
-                right: 7.0,
-                top: 50.0,
-                bottom: 10.0,
-              ),
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  minimumSize: const Size(350, 450),
-                  primary: colorBlue2,
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text('bKind'),
+            backgroundColor: colorDarkBlue,
+          ),
+          drawer: Drawer(
+            child: ListView(
+              padding: EdgeInsets.zero,
+              children: <Widget>[
+                Container(
+                  height: 100.0,
+                  child: DrawerHeader(
+                    decoration: const BoxDecoration(
+                      color: colorDarkBlue,
+                    ),
+                    child: Text(
+                      'Hello,  ${loggedInUser.name}',
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 24,
+                      ),
+                    ),
+                  ),
                 ),
-                child: const Text('Call first available user'),
-                onPressed: () async {
-                  print("calling");
-                  await Permissions.cameraAndMicrophonePermissionsGranted()
-                      ? CallUtils.dial(
-                          from: loggedInUser, to: toCallUser, context: context)
-                      : print("no permission");
+                ListTile(
+                  leading: IconButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const Profile()));
+                    },
+                    icon: const Icon(Icons.account_circle),
+                  ),
+                  title: const Text('Profile'),
+                ),
+                ListTile(
+                  leading: IconButton(
+                    onPressed: () {
+                      logout(context);
+                    },
+                    icon: const Icon(Icons.logout),
+                  ),
+                  title: const Text('Logout'),
+                ),
+
+                // ignore: prefer_const_constructors
+              ],
+            ),
+          ),
+          body: SingleChildScrollView(
+            child: Column(children: [
+              Container(
+                alignment: Alignment.topCenter,
+                margin: const EdgeInsets.only(
+                  left: 7.0,
+                  right: 7.0,
+                  top: 50.0,
+                  bottom: 10.0,
+                ),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: const Size(350, 450),
+                    primary: colorBlue2,
+                  ),
+                  child: const Text('Call first available user'),
+                  onPressed: () async {
+                    print("calling");
+                    await Permissions.cameraAndMicrophonePermissionsGranted()
+                        ? CallUtils.dial(
+                            from: loggedInUser,
+                            to: toCallUser,
+                            context: context)
+                        : print("no permission");
+                  },
+                ),
+              ),
+            ]),
+          ),
+          bottomNavigationBar: BottomAppBar(
+              child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              IconButton(
+                icon: const Icon(Icons.home),
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const HomeBlind()));
                 },
               ),
-            ),
-          ]),
+              IconButton(
+                icon: const Icon(Icons.account_circle),
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => const Profile()));
+                },
+              ),
+            ],
+          )),
         ),
-        bottomNavigationBar: BottomAppBar(
-            child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            IconButton(
-              icon: const Icon(Icons.home),
-              onPressed: () {},
-            ),
-            IconButton(
-              icon: const Icon(Icons.dialpad_outlined),
-              // alignment: Center,
-              onPressed: () {},
-            ),
-            IconButton(
-              icon: const Icon(Icons.settings),
-              onPressed: () {},
-            ),
-          ],
-        )),
       ),
     );
+  }
+
+  Future<void> logout(BuildContext context) async {
+    await FirebaseAuth.instance.signOut();
+    Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => const Login()));
   }
 }
--- lib/screens/home_volunteer.dart
+++ lib/screens/home_volunteer.dart
@@ -1,6 +1,7 @@
 import 'package:bkind/models/user_model.dart';
 import 'package:bkind/provider/user_provider.dart';
 import 'package:bkind/screens/callscreens/pickup/pickup_layout.dart';
+import 'package:bkind/screens/login.dart';
 import 'package:bkind/screens/profile.dart';
 import 'package:bkind/utils/call_utilis.dart';
 import 'package:bkind/utils/constants.dart';
@@ -74,127 +75,180 @@ class _HomeVolunteerState extends State<HomeVolunteer> {
 
   @override
   Widget build(BuildContext context) {
-    return PickupLayout(
-      scaffold: MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-            title: const Text('bKind'),
-            backgroundColor: colorDarkBlue,
-          ), //AppBar
-          body: SingleChildScrollView(
-            child: Padding(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  // Container(
-                  //   margin: EdgeInsets.only(
-                  //     left: 7.0,
-                  //     top: 25.0,
-                  //   ),
-                  //   child: Text(
-                  //     "Hello user_name"
-                  //   ),
-                  // ),
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: PickupLayout(
+        scaffold: MaterialApp(
+          home: Scaffold(
+            appBar: AppBar(
+              title: const Text('bKind'),
+              backgroundColor: colorDarkBlue,
+            ),
+            drawer: Drawer(
+              child: ListView(
+                padding: EdgeInsets.zero,
+                children: <Widget>[
                   Container(
-                    // width: 200.0,
-                    // height: 100.0,
-
-                    child: Text(
-                      "Hello, ${loggedInUser.name}",
-                      style: const TextStyle(fontSize: 20),
-                    ),
-                  ),
-                  Container(
-                    // width: 200.0,
-                    // height: 100.0,
-                    margin: const EdgeInsets.only(
-                      // left: 10,
-                      top: 5,
-                      bottom: 20,
-                    ),
-                    child: const Text(
-                      "Welcome to bKind !",
-                      style: TextStyle(fontSize: 20),
-                    ),
-                  ),
-                  Container(
-                    height: 250.0,
-                    width: 300.0,
-                    color: Colors.transparent,
-                    // margin: EdgeInsets.all(20),
-                    child: Container(
+                    height: 100.0,
+                    child: DrawerHeader(
                       decoration: const BoxDecoration(
-                          color: colorBlue2,
-                          borderRadius:
-                              BorderRadius.all(Radius.circular(10.0))),
-                      child: Center(
-                        child: Image.asset(
-                          'assets/earth.png',
-                          width: 200,
-                          height: 200,
+                        color: colorDarkBlue,
+                      ),
+                      child: Text(
+                        'Hello,  ${loggedInUser.name}',
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 24,
                         ),
                       ),
                     ),
                   ),
-                  Container(
-                    // alignment: Alignment.topCenter,
-                    margin: const EdgeInsets.only(
-                      // left: 7.0,
-                      // right: 7.0,
-                      top: 140.0,
-                      // bottom: 10.0,
-                    ),
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        minimumSize: const Size(300, 50),
-                        primary: colorBlue4,
-                      ),
-                      child: const Text('Learn how to call'),
-                      onPressed: () async {
-                        print("calling");
-                        await Permissions
-                                .cameraAndMicrophonePermissionsGranted()
-                            ? CallUtils.dial(
-                                from: loggedInUser,
-                                to: toCallUser,
-                                context: context)
-                            : print("no permission");
+                  ListTile(
+                    leading: IconButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const Profile()));
                       },
+                      icon: const Icon(Icons.account_circle),
                     ),
+                    title: const Text('Profile'),
+                  ),
+                  ListTile(
+                    leading: IconButton(
+                      onPressed: () {
+                        logout(context);
+                      },
+                      icon: const Icon(Icons.logout),
+                    ),
+                    title: const Text('Logout'),
+                  ),
+                  // ignore: prefer_const_constructors
+                ],
+              ),
+            ), //AppBar
+            body: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 20.0, vertical: 20.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    // Container(
+                    //   margin: EdgeInsets.only(
+                    //     left: 7.0,
+                    //     top: 25.0,
+                    //   ),
+                    //   child: Text(
+                    //     "Hello user_name"
+                    //   ),
+                    // ),
+                    Container(
+                      // width: 200.0,
+                      // height: 100.0,
+
+                      child: Text(
+                        "Hello, ${loggedInUser.name}",
+                        style: const TextStyle(fontSize: 20),
+                      ),
+                    ),
+                    Container(
+                      // width: 200.0,
+                      // height: 100.0,
+                      margin: const EdgeInsets.only(
+                        // left: 10,
+                        top: 5,
+                        bottom: 20,
+                      ),
+                      child: const Text(
+                        "Welcome to bKind !",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ),
+                    Container(
+                      height: 250.0,
+                      width: 300.0,
+                      color: Colors.transparent,
+                      // margin: EdgeInsets.all(20),
+                      child: Container(
+                        decoration: const BoxDecoration(
+                            color: colorBlue2,
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(10.0))),
+                        child: Center(
+                          child: Image.asset(
+                            'assets/earth.png',
+                            width: 200,
+                            height: 200,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      // alignment: Alignment.topCenter,
+                      margin: const EdgeInsets.only(
+                        // left: 7.0,
+                        // right: 7.0,
+                        top: 140.0,
+                        // bottom: 10.0,
+                      ),
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          minimumSize: const Size(300, 50),
+                          primary: colorBlue4,
+                        ),
+                        child: const Text('Learn how to call'),
+                        onPressed: () async {
+                          print("calling");
+                          await Permissions
+                                  .cameraAndMicrophonePermissionsGranted()
+                              ? CallUtils.dial(
+                                  from: loggedInUser,
+                                  to: toCallUser,
+                                  context: context)
+                              : print("no permission");
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            bottomNavigationBar: BottomAppBar(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                    icon: const Icon(Icons.home),
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const HomeVolunteer()));
+                    },
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.settings),
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const Profile()));
+                    },
                   ),
                 ],
               ),
             ),
           ),
-          bottomNavigationBar: BottomAppBar(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                IconButton(
-                  icon: const Icon(Icons.home),
-                  onPressed: () {},
-                ),
-                IconButton(
-                  icon: const Icon(Icons.dialpad_outlined),
-                  // alignment: Center,
-                  onPressed: () {},
-                ),
-                IconButton(
-                  icon: const Icon(Icons.settings),
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const Profile()));
-                  },
-                ),
-              ],
-            ),
-          ),
         ),
       ),
     );
+  }
+
+  Future<void> logout(BuildContext context) async {
+    await FirebaseAuth.instance.signOut();
+    Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => const Login()));
   }
 }
